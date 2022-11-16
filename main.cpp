fae86c6cb56cb884226cc123d1fc8ed131835121
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2022-11-16 16:28:52 +0100

    small update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 I2C i2c(I2C_SDA, I2C_SCL);
 
 int main() {
+     // Make PA_11 and PA_12 floating that we won't short anything when connecting SDA->PA12 and SCL->PA11
+    DigitalIn _(PA_11);
+    DigitalIn __(PA_12);
+    
     hwlcd lcd(&i2c, i2c_find_first_device(i2c));
     lcd.init(true, false);
     lcd.puts("Hewoo");
